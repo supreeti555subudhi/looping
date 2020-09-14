1c376fd3b7a330ddb2814db7f6f0dcf37942ad57
supreeti555subudhi <71143747+supreeti555subudhi@users.noreply.github.com>
2020-09-15 01:17:49 +0530

    created to print positive numbers
    this program prints all the positive number from the lists

---
+++ positive.py
@@ -0,0 +1,13 @@
+list1 = [12,-7,5,64,-14]
+
+for n in list1:
+
+    if n >= 0:
+       print(n)
+print("\n")
+list2 = [12,14,-95,3] 
+ 
+for num in list2: 
+       
+    if num >= 0:
+       print(num) 
