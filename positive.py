bd4dac8cd657ff4fb3f961e505f8aa72ee710ce6
supreeti555subudhi <71143747+supreeti555subudhi@users.noreply.github.com>
2020-09-22 20:10:47 +0530

    created positive.py
    extracts the positive no. from list provided by the user

--- positive.py
+++ positive.py
@@ -2,12 +2,12 @@ list1 = [12,-7,5,64,-14]
 
 for n in list1:
 
-    if n >= 0:
+    if n >= 1:
        print(n)
 print("\n")
 list2 = [12,14,-95,3] 
  
 for num in list2: 
        
-    if num >= 0:
+    if num >= 1:
        print(num) 
